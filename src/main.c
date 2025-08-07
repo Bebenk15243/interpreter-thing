07d7da187ead25d323260deec0f3a151375a82a0
Bebenk15243 <pepijn.simoens@gmail.com>
2025-08-07 18:34:12 +0200

    fixed seg fault of (if statement

--- src/main.c
+++ src/main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+// #include "memory.h"
+
+#define BUFFERSIZE 2047
+
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+
+char* select(char* buffer, u16 start_index, u16 end_index) {
+return 0;
+}
+
+u8 is_if_expression(char* buffer) {
+	char substring[5];
+	strncpy(substring, buffer, 4);
+	substring[4] = '\0';
+	if (!strcmp(substring, "(if ")) {
+		fputs("got if\n", stdout);
+
+	}
+	return 0;
+}
+
 
 int main(const int argc, const char **argv)
-{
-    printf("hello world\n");
-    return 0;
+{	
+	char input_buffer[BUFFERSIZE];
+	uint8_t running = 1;
+	while (running) {
+		fputs("lisp> ", stdout);
+		fgets(input_buffer, BUFFERSIZE, stdin);
+		if (strcmp(input_buffer, "(exit)\n") == 0) {running = 0;}
+		
+		is_if_expression(input_buffer);
+	}
+	fputs("exiting...\n", stdout);
+	return 0;
 }
