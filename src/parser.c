82cbaec2fdc56a2e61fd87e2928a2ed4feb37db2
Bebenk15243 <pepijn.simoens@gmail.com>
2026-03-24 22:32:35 +0100

    comments: in parser to explain main ideas of functions and weird quircks

--- src/parser.c
+++ src/parser.c
@@ -4,8 +4,12 @@
 #define WARN_FLAG 1
 #define DEBUG_FLAG 1
 
-// WARNING: FIX FREE OF POINTERS AFTER *parse AS THEY ALREADY ALLOCATE MEMORY
+// TODO:
+// - free for all ast structures
+// - comments
 
+// checks if there is an RPAR token to close the expression.
+// also updates the index
 void has_RPAR(int *index, token *tokenarray) {
     if (tokenarray[*index].token_id != RPAR) {
         PRINT_ERROR("expected a RPAR");
@@ -13,10 +17,16 @@ void has_RPAR(int *index, token *tokenarray) {
     }
     (*index)++;
 }
+
+// lambdas look like this: (lambda (<param(s)>) <expr>)
+// lpar and lambda should be parsed by the parse function.
 lambda_exp *parse_lamdba(int *index, token *tokenarray) {
     lambda_exp *lambda = (lambda_exp *)malloc(sizeof(lambda_exp));
     // for paremeter parsing but needs a parenthesis
     (*index)++;
+
+    // checks if correct syntax is used: an opening parenthesis for the
+    // parameter(s)
     if (tokenarray[(*index)++].token_id != LPAR) {
         PRINT_ERROR("parser: lambda where left parenthesis for parameter");
         exit(1);
@@ -26,9 +36,15 @@ lambda_exp *parse_lamdba(int *index, token *tokenarray) {
     if (tokenarray[(*index)].token_id == RPAR) {
         PRINT_DEBUG("ITS A THUNNNKKK");
         lambda->parameters = NULL;
+        // I think I wrote this because it also updates the index but not sure
+        // TODO: check why this is here
         has_RPAR(index, tokenarray);
     } else {
+        // if no RPAR is found,
         // parse the parameters
+        // parameters are just a list of variables
+        // (arg1 arg2 arg3 ...)
+        //
         //  first counting the parameters
         PRINT_DEBUG("checking parameters");
         int length = 0;
@@ -40,6 +56,7 @@ lambda_exp *parse_lamdba(int *index, token *tokenarray) {
             }
             length++;
         }
+        // allocating space for the parameters and copy in to the array
         lambda->parameters = (var_exp *)calloc(length, sizeof(var_exp));
         PRINT_DEBUG("found %d parameters: ", length);
         for (int param_index = 0; param_index < length; param_index++) {
@@ -48,22 +65,39 @@ lambda_exp *parse_lamdba(int *index, token *tokenarray) {
             PRINT_DEBUG("parameter %d: %s", param_index + 1,
                         lambda->parameters[param_index].name);
         }
+        // update the index
         *index += length;
         PRINT_DEBUG("parameters are checked and assigned");
+
+        // end of the parameter list is an `)`
         has_RPAR(index, tokenarray);
     }
-    lambda->expression = *parse(index, tokenarray); // parse the expression part
+    // the expression part will be parsed recursive, not the best option but
+    // certainly the easiest...
+    lambda->expression = *parse(index, tokenarray);
+
+    // lambda expressions end with `)`
     has_RPAR(index, tokenarray);
     return lambda;
 }
 
+// if expressions: (if <condition> <consequent> <alternative>)
 if_exp *parse_if(int *index, token *tokenarray) {
+    // make space for if expression
+    //  also, as if is a keyword in C, I had to use if_ which I find goofy but
+    //  its only for this function
     if_exp *if_ = (if_exp *)malloc(sizeof(if_exp));
     (*index)++;
+
+    // The opening Lpar and if does not have to be parsed, done by parse
+    // function.
+    // next is the condition, this can be an expression so we use parse recursif
     expr_t *temp = parse(index, tokenarray);
     if_->condition = *temp;
     free(temp);
 
+    // same structure is used
+    // TODO: refactor these three expressions?
     temp = parse(index, tokenarray);
     if_->consequent = *temp;
     free(temp);
@@ -72,17 +106,31 @@ if_exp *parse_if(int *index, token *tokenarray) {
     if_->alternative = *temp;
     free(temp);
 
+    // ending RPAR of the if expression
     has_RPAR(index, tokenarray);
     return if_;
 }
 
+// aplications are expressions without the special tokens in the begin:
+//  (<var> [<params>]*)
 application_exp *parse_application(int *index, token *tokenarray) {
     application_exp *app = (application_exp *)malloc(sizeof(application_exp));
+    // parsing the procedure part, which can be another application or lambda or
+    // anything realy.
+    //  ((<var>) 1) has to work
     app->procedure = *parse(index, tokenarray);
     PRINT_DEBUG("found a procedure?: %s",
                 ((var_exp *)app->procedure.expr)->name);
 
     // NOTE: realloc allong the way by multiples of 2
+
+    // Parameters can be expressions.
+    // they are parsed using a loop.
+    // a realloc is used as there is nothing to say how many arguments there are
+    // given some space is allocated and will expand when too small. this is
+    // accompished using 2 variables, one the args index and one the arg count
+    // the count is how much space is allocated and the index to index into this
+    // array.
     int args_index = 0;
     // initial allocation (in this case 4)
     app->arg_count = 4; // TODO: make an macro definition for this
@@ -90,6 +138,7 @@ application_exp *parse_application(int *index, token *tokenarray) {
     // start counting
     while (tokenarray[*index].token_id != RPAR) {
         // if there is more memory necessary <- this word is too hard to write
+        //  then reallocate space
         if (args_index == app->arg_count) {
             app->args = (expr_t *)realloc(app->args, app->arg_count * 2);
             app->arg_count *= 2;
@@ -97,16 +146,26 @@ application_exp *parse_application(int *index, token *tokenarray) {
         app->args[args_index] = *parse(index, tokenarray);
         args_index++;
     }
+
+    // resize to just fit the amount of arguments
     app->args = (expr_t *)realloc(app->args, args_index);
     app->arg_count = args_index;
+
+    // applications also end with a RPAR
     has_RPAR(index, tokenarray);
     return app;
 }
 
+// the big boi, should parse anything
+// gives back an expression... this makes it handy to use recursively altho not
+// the most efficient should be rewritten so labels and gotos can be used
 expr_t *parse(int *index, token *tokenarray) {
+    // this is pretty straight forward
     expr_t *exp = (expr_t *)malloc(sizeof(expr_t));
     switch (tokenarray[*index].token_id) {
     case LPAR:
+        // TODO: this case can be refactored so only a single swich statement is
+        // used...
         PRINT_DEBUG("LPAR parsed");
         *index += 1;
         if (tokenarray[*index].token_id == LAMBDA) {
@@ -116,6 +175,9 @@ expr_t *parse(int *index, token *tokenarray) {
             break;
         } else if (tokenarray[*index].token_id == DEFINE) {
             PRINT_DEBUG("found define");
+            // TODO: implement the define
+            // NOTE: as a functional bro I dont think we need this, prove me
+            // wrong... go nuts
             break;
         } else if (tokenarray[*index].token_id == IF) {
             PRINT_DEBUG("found if");
@@ -127,6 +189,7 @@ expr_t *parse(int *index, token *tokenarray) {
             exp->expr = parse_application(index, tokenarray);
             break;
         } else if (tokenarray[*index].token_id == LPAR) {
+            // expression in expression, never know what it is ...
             exp->type = APP;
             exp->expr = parse(index, tokenarray);
             break;
